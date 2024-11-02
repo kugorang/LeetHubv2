a4fcc4067b76747cb2e66413724dfcb0728a92d0
Kim Hyeonwoo <ialskdji@gmail.com>
2024-11-02 14:37:28 +0900

    Time: 28 ms (50.53%), Space: 39.4 MB (89.89%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        char previousChar;
-        bool isFancyCondition;
         string answer;
+        bool isFancyCondition;
+        char previousChar;
 
         answer.reserve(s.size());
 
         for (auto ch : s)
         {
-            if (previousChar == ch && isFancyCondition)
+            if (isFancyCondition && previousChar == ch)
                 continue;
     
             answer.push_back(ch);
 
-            if (previousChar == ch)
+            if (previousChar != ch)
             {
-                isFancyCondition = true;
-                continue;
+                previousChar = ch;
+                isFancyCondition = false;
             }
-
-            previousChar = ch;
-            isFancyCondition = false;
+            else
+                isFancyCondition = true;
         }
 
         return answer;
