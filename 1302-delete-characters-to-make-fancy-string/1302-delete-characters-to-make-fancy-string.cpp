acadc7125a18baff844ff031fc05f57e4abb0044
Kim Hyeonwoo <ialskdji@gmail.com>
2024-11-02 14:22:32 +0900

    Time: 14 ms (94.74%), Space: 42.8 MB (31.24%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        char previousChar = '.';
+        bool isFancyCondition;
+        string answer;
+
+        for (auto ch : s)
+        {
+            if (isFancyCondition && previousChar == ch)
+                continue;
+    
+            answer.push_back(ch);
+
+            if (previousChar != ch)
+            {
+                previousChar = ch;
+                isFancyCondition = false;
+            }
+            else
+                isFancyCondition = true;
+        }
+
+        return answer;
+    }
+};
