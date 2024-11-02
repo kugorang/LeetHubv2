79a79b2192cde321f2993639db6ef0a0274bb431
Kim Hyeonwoo <ialskdji@gmail.com>
2024-11-02 14:40:03 +0900

    Time: 15 ms (92.63%), Space: 39.2 MB (91.01%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        char previousChar = '@';
+        char previousChar = '.';
         bool isFancyCondition;
         string answer;
 
