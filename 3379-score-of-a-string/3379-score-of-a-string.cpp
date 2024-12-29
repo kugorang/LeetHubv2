658bc866f0d29f8ec4a2df0424e3c5508bec5435
Kim Hyeonwoo <ialskdji@gmail.com>
2024-12-29 22:22:44 +0900

    Time: 0 ms (100%), Space: 9 MB (15.7%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        auto result = 0;
-        auto i = 0;
+        unsigned short result = 0;
+        size_t i = 0;
 
-        while (s[++i]) {
-            if (s[i] > s[i - 1])
-                result += (s[i] - s[i - 1]);
-            else
-                result += (s[i - 1] - s[i]);
-        }
+        while (s[++i])
+            result += abs(s[i] - s[i - 1]);
 
         return result;
     }
