32ec10d5ad62158f32f09bdff1f7d1c82b995786
Kim Hyeonwoo <ialskdji@gmail.com>
2024-12-29 22:23:07 +0900

    Time: 0 ms (100%), Space: 9 MB (15.7%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -4,8 +4,12 @@ public:
         unsigned short result = 0;
         size_t i = 0;
 
-        while (s[++i])
-            result += abs(s[i] - s[i - 1]);
+        while (s[++i]) {
+            if (s[i] > s[i - 1])
+                result += (s[i] - s[i - 1]);
+            else
+                result += (s[i - 1] - s[i]);
+        }
 
         return result;
     }
