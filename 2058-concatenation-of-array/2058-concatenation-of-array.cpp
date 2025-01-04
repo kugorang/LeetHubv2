66228f48d6a4d7fbcbf1303942eb57a42fbcb194
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-05 06:40:36 +0900

    Time: 0 ms (100%), Space: 16.8 MB (42.36%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> result(nums);
+        vector<int> result;
         result.reserve(nums.size() << 1);
 
-        for (auto num : nums)
-            result.push_back(num);
+        for (int i = 0; i < 2; ++i)
+            for (auto num : nums)
+                result.push_back(num);
         
         return result;
     }
