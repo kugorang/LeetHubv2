a096f6d820dd2e99b6fdb06743e6cb8eec32f455
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-08 17:28:29 +0900

    Time: 0 ms (100%), Space: 8.1 MB (67.08%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.cpp
+++ 2556-convert-the-temperature/2556-convert-the-temperature.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        vector<double> answer;
-        answer.reserve(2);
-
-        answer.push_back(celsius + 273.15);
-        answer.push_back(celsius * 1.8 + 32.0);
-
-        return answer;
+        return vector<double> {celsius + 273.15, celsius * 1.8 + 32.0};
     }
 };
