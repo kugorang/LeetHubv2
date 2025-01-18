6b89d427a18578aab083990271c35f37c34c09fa
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-18 23:02:34 +0900

    Time: 0 ms (100%), Space: 7.7 MB (85.1%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int count = 0;
+        int answer = 0;
 
         while (num > 0)
         {
-            num = num % 2 ? num - 1 : num * 0.5f;
-            ++count;
+            num = num % 2 ? num - 1 : num / 2;
+            ++answer;
         }
-
-        return count;
+        
+        return answer;
     }
 };
