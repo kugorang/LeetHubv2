d17e8baadbca9ca77999ce18e20e85619bc8db88
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-19 22:33:50 +0900

    Time: 0 ms (100%), Space: 8.4 MB (59.91%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        unsigned int num1 = 0, num2 = 0;
+        unsigned int result = 0;
 
         for (unsigned short i = 1; i <= n; ++i)
-        {
-            if (i % m)
-                num1 += i;
-            else
-                num2 += i;
-        }
+            result = (i % m) ? result + i : result - i;
 
-        return num1 - num2;
+        return result;
     }
 };
