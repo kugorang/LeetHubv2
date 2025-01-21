e3e45830270fe8f27bf9dd42d29dbf54c1e64b32
Kim Hyeonwoo <ialskdji@gmail.com>
2025-01-21 23:28:23 +0900

    Time: 0 ms (100%), Space: 8.1 MB (95.65%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.cpp
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -3,8 +3,8 @@ public:
     int numJewelsInStones(string jewels, string stones) {
         size_t answer = 0;
 
-        for (size_t jewel : jewels)
-            for (auto stone : stones)
+        for (size_t stone : stones)
+            for (size_t jewel : jewels)
                 if (jewel == stone)
                     ++answer;
 
