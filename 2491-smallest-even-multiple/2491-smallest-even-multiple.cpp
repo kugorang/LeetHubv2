1d3a022e4ce1108f58d534483b80a9d9aa997746
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-02 19:01:56 +0900

    Time: 0 ms (100%), Space: 7.9 MB (29.01%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if (n % 2 == 0)
-            return (n);
-        return (n << 1);
+        return (n % 2 == 0) ? n : (n << 1);
     }
 };
