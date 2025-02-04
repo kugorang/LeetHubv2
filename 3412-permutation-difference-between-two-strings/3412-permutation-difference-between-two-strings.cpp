8a24d67427ccfac62d4cb7282f96011f6609176f
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-04 14:05:33 +0900

    Time: 0 ms (100%), Space: 8.8 MB (98.46%) - LeetHub

---
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        size_t sLen = s.length();
+        size_t tLen = t.length();
+        int answer = 0;
+
+        for (int i = 0; i < sLen; ++i)
+        {
+            for (int j = 0; j < tLen; ++j)
+            {
+                if (s[i] != t[j])
+                    continue;
+
+                answer += abs(i - j);
+                break;
+            }
+        }
+
+        return answer;
+    }
+};
