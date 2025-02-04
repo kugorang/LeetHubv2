a62414076c01134b8444bc2d08a770fddde234a7
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-04 14:09:17 +0900

    Time: 0 ms (100%), Space: 8.8 MB (98.46%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.cpp
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.cpp
@@ -9,11 +9,11 @@ public:
         {
             for (int j = 0; j < tLen; ++j)
             {
-                if (s[i] == t[j])
-                {
-                    answer += (i >= j ? i - j : j - i);
-                    break;
-                }
+                if (s[i] != t[j])
+                    continue;
+
+                answer += abs(i - j);
+                break;
             }
         }
 
