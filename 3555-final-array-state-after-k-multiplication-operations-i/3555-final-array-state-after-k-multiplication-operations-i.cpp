42262560c42427f2ff57dbe57e0393b777563127
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-08 17:01:42 +0900

    Time: 0 ms (100%), Space: 27.2 MB (80.02%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        for (size_t i = 0; i < k; ++i)
+        {
+            size_t numsSize = nums.size();
+            size_t minIndex = 0;
+
+            for (size_t j = 1; j < numsSize; ++j)
+                if (nums[minIndex] > nums[j])
+                    minIndex = j;
+            
+            nums[minIndex] *= multiplier;
+        }
+
+        return nums;
+    }
+};
