d655bed10950dc5a9a3d048f2cfdfb8bb77912e1
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-18 23:40:33 +0900

    Time: 0 ms (100%), Space: 18.9 MB (47.41%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.cpp
+++ 1651-shuffle-string/1651-shuffle-string.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        unsigned short sLen = s.length();
-        string answer(sLen, '\0');
+        unsigned short indicesSize = indices.size();
+        string answer(indicesSize, '\0');
 
-        for (unsigned short i = 0; i < sLen; ++i)
+        for (unsigned short i = 0; i < indicesSize; ++i)
             answer[indices[i]] = s[i];
 
         return answer;
