144a306f978d47b970ca4176e7df21139605e574
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-21 23:23:37 +0900

    Time: 0 ms (100%), Space: 9.1 MB (17.25%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.cpp
+++ 1944-truncate-sentence/1944-truncate-sentence.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     string truncateSentence(string s, int k) {
         unsigned short spaceCount = 0;
-        unsigned short answerIndex = 0;
         unsigned short sLength = s.length();
 
         for (unsigned short i = 0; i < sLength; ++i)
-        {
             if (s[i] == ' ')
                 if (++spaceCount == k)
-                    return s.substr(0, answerIndex);
-            ++answerIndex;
-        }
+                    return s.substr(0, i);
 
         return s;
     }
