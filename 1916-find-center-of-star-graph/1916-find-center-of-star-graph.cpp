909dbd95c8f837b5fdb95b570859890f892adcf0
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-24 16:48:52 +0900

    Time: 0 ms (100%), Space: 98.9 MB (10.6%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    Solution()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    
     int findCenter(vector<vector<int>>& edges) {
+
         if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
             return edges[0][0];
         
