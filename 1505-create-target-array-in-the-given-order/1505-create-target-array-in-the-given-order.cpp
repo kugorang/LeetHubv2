89313c0f757266bdc52642719a72aeaf8187f68a
Kim Hyeonwoo <ialskdji@gmail.com>
2025-02-25 11:51:14 +0900

    Time: 0 ms (100%), Space: 11.4 MB (65.29%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.cpp
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        int numsSize = nums.size();
+        unsigned char numsSize = nums.size();
 
         vector<int> target;
         target.reserve(numsSize);
-        
+
         vector<int>::iterator targetIt = target.begin();
 
-        for (int i = 0; i < numsSize; ++i)
+        for (unsigned char i = 0; i < numsSize; ++i)
             target.insert(targetIt + index.at(i), nums.at(i));
             
         return target;
