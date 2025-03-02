030900a43778ca4df81781727d6d394df5f8bc98
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-02 20:07:03 +0900

    Time: 0 ms (100%), Space: 36.4 MB (36.64%) - LeetHub

--- 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.cpp
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
         int answer = 0;
+        unsigned short index;
+        
+        if (ruleKey == "type")
+            index = 0;
+        else if (ruleKey == "color")
+            index = 1;
+        else
+            index = 2;
 
         for (auto &item : items)
-        {
-            unsigned short index;
-            
-            if (ruleKey == "type")
-                index = 0;
-            else if (ruleKey == "color")
-                index = 1;
-            else
-                index = 2;
-
             if (item[index] == ruleValue)
                 ++answer;
-        }
-            
 
         return answer;
     }
