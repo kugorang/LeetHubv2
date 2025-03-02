2bc8e0187b2fb8971713955e4f4b3b8e2b86c7b7
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-02 19:58:20 +0900

    Time: 3 ms (30.73%), Space: 36.4 MB (36.64%) - LeetHub

---
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    unordered_map<string, int> um;
+public:
+    Solution()
+    {
+        um = {
+            {"type", 0},
+            {"color", 1},
+            {"name", 2}
+        };
+    }
+
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue)
+    {
+        int answer = 0;
+
+        for (auto &item : items)
+            if (item[um[ruleKey]] == ruleValue)
+                ++answer;
+
+        return answer;
+    }
+};
