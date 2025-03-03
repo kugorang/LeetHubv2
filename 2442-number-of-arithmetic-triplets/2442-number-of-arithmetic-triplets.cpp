39275704a687d36da25bb9ac30550b98e49a0821
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-03 15:57:19 +0900

    Time: 0 ms (100%), Space: 11.7 MB (70.94%) - LeetHub

--- 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/2442-number-of-arithmetic-triplets.cpp
@@ -5,10 +5,18 @@ public:
         int answer = 0;
 
         for (unsigned char i = 0; i < numsSize - 2; ++i)
+        {
             for (unsigned char j = i + 1; j < numsSize - 1; ++j)
+            {
+                if (nums[j] - nums[i] != diff)
+                    continue;
+
                 for (unsigned char k = j + 1; k < numsSize; ++k)
-                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
+                    if (nums[k] - nums[j] == diff)
                         ++answer;
+            }
+        }
+            
 
         return answer;
     }
