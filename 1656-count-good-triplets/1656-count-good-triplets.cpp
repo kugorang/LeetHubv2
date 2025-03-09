16276e5fa877e725412098b28176a59295b083eb
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-09 21:08:18 +0900

    Time: 3 ms (98.36%), Space: 11.1 MB (41.92%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -1,9 +1,4 @@
 class Solution {
-private:
-    int m_abs(int num)
-    {
-        return num >= 0 ? num : -num;
-    }
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) 
     {
@@ -14,11 +9,11 @@ public:
         {
             for (int j = i + 1; j < arrSize - 1; ++j)
             {
-                if (m_abs(arr[i] - arr[j]) > a)
+                if (abs(arr[i] - arr[j]) > a)
                     continue;
 
                 for (int k = j + 1; k < arrSize; ++k)
-                    if (m_abs(arr[j] - arr[k]) <= b && m_abs(arr[i] - arr[k]) <= c)
+                    if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
                         ++answer;
             }
         }
