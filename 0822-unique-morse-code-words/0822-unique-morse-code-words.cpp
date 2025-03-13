124a5602053b16454b5474dc3d7372373859b011
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-13 12:39:52 +0900

    Time: 0 ms (100%), Space: 11.8 MB (6.94%) - LeetHub

---
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> morse = {
+            ".-","-...","-.-.","-..",".","..-.","--.",
+            "....","..",".---","-.-",".-..","--","-.","---",".--.",
+            "--.-",".-.","...","-","..-","...-",
+            ".--","-..-","-.--","--.."
+        };
+        unordered_map<string, bool> um;
+
+        for (auto &word : words)
+        {
+            string currentMorse = "";
+
+            for (auto &ch : word)
+                currentMorse += morse[ch - 'a'];
+            
+            um.insert(make_pair(currentMorse, true));
+        }
+
+        int answer = 0;
+
+        for (auto &element : um)
+            if (element.second)
+                ++answer;
+
+        return answer;
+    }
+};
