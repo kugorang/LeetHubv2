504005076d3a1904c4219c741a848b91670133f5
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-19 16:44:13 +0900

    Time: 0 ms (100%), Space: 7.8 MB (52.99%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int answer = 0;
+
+        for (int i = 0; i < n; ++i)
+            answer ^= start + (i << 1);
+            
+        return answer;
+    }
+};
