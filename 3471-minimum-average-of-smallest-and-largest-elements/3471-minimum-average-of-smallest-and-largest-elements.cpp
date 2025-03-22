6be7ef73d6c5d8b4f5fdc7be05b64751fde8ea2b
Kim Hyeonwoo <ialskdji@gmail.com>
2025-03-22 15:24:28 +0900

    Time: 0 ms (100%), Space: 26.3 MB (8.17%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -3,32 +3,17 @@ public:
     double minimumAverage(vector<int>& nums)
     {
         unsigned char numsSize = nums.size();
+        sort(nums.begin(), nums.end());
+
         vector<double> averages;
         averages.reserve(numsSize >> 1);
 
         while (numsSize > 0)
         {
-            unsigned char max = 0;
-            unsigned char min = 51;
+            averages.push_back((double)(nums[0] + nums[numsSize - 1]) / 2);
 
-            unsigned char minIndex = -1;
-
-            for (unsigned char i = 0; i < numsSize; ++i)
-            {
-                if (min > nums[i])
-                {
-                    min = nums[i];
-                    minIndex = i;
-                }
-
-                if (nums[i] > max)
-                    max = nums[i];
-            }
-
-            averages.push_back((double)(min + max) / 2);
-
-            nums.erase(nums.begin() + minIndex);
-            nums.erase(find(nums.begin(), nums.end(), max));
+            nums.erase(nums.begin() + numsSize - 1);
+            nums.erase(nums.begin());
 
             numsSize -= 2;
         }
