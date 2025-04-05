deaa2cc4b3b05753f997d0899ac16e553f6d1bbb
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-05 11:01:46 +0900

    Time: 99 ms (10.47%), Space: 13.9 MB (92.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,15 +8,21 @@ public:
 
         for (unsigned short i = 0; i < numsSize - 1; ++i)
         {
+            bool found = false;
+
             for (unsigned short j = i + 1; j < numsSize; ++j)
             {
                 if (nums[i] + nums[j] == target)
                 {
+                    found = true;
                     answer.push_back(i);
                     answer.push_back(j);
                     break;
                 }
             }
+
+            if (found)
+                break;
         }
 
         return answer;
