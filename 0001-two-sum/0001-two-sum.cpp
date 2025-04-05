0b4dd23f3a9c4b63a900bd56dfca58454d981f45
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-05 11:13:49 +0900

    Time: 36 ms (36.79%), Space: 14.1 MB (61.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> answer;
-        answer.reserve(2);
-
         int numsSize = nums.size();
 
         for (int i = 0; i < numsSize - 1; ++i)
-        {
-            bool found = false;
-
             for (int j = i + 1; j < numsSize; ++j)
-            {
                 if (nums[i] + nums[j] == target)
-                {
-                    found = true;
-                    answer.push_back(i);
-                    answer.push_back(j);
-                    break;
-                }
-            }
+                    return {i, j};
 
-            if (found)
-                break;
-        }
-
-        return answer;
+        return {};
     }
 };
