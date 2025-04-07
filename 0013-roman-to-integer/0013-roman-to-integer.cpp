e0b184a140c14bb126a2e7390b1bee612c25cc95
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-07 11:16:18 +0900

    Time: 0 ms (100%), Space: 10 MB (84.42%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sLength = s.length();
+        int answer = 0;
+
+        for (int i = 0; i < sLength; ++i)
+        {
+            if (s[i] == 'M')
+                answer += 1000;
+            else if (s[i] == 'D')
+                answer += 500;
+            else if (s[i] == 'C')
+            {
+                if (i < sLength - 1)
+                {
+                    if (s[i + 1] == 'D')
+                    {
+                        answer += 400;
+                        ++i;
+                    }
+                    else if (s[i + 1] == 'M')
+                    {
+                        answer += 900;
+                        ++i;
+                    }
+                    else
+                        answer += 100;
+                }
+                else
+                    answer += 100;
+            }
+            else if (s[i] == 'L')
+                answer += 50;
+            else if (s[i] == 'X')
+            {
+                if (i < sLength - 1)
+                {
+                    if (s[i + 1] == 'L')
+                    {
+                        answer += 40;
+                        ++i;
+                    }
+                    else if (s[i + 1] == 'C')
+                    {
+                        answer += 90;
+                        ++i;
+                    }
+                    else
+                        answer += 10;
+                }
+                else
+                    answer += 10;
+            }
+            else if (s[i] == 'V')
+                answer += 5;
+            else if (s[i] == 'I')
+            {
+                if (i < sLength - 1)
+                {
+                    if (s[i + 1] == 'V')
+                    {
+                        answer += 4;
+                        ++i;
+                    }
+                    else if (s[i + 1] == 'X')
+                    {
+                        answer += 9;
+                        ++i;
+                    }
+                    else
+                        answer += 1;
+                }
+                else
+                    answer += 1;
+            }
+        }
+
+        return answer;
+    }
+};
