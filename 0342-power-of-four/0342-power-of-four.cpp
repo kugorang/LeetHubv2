681f9d38ec7207e8e5238da9ffe48d36ab1a25a6
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-11 16:50:59 +0900

    Time: 0 ms (100%), Space: 7.7 MB (83.46%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n == 1)
-            return true;
-
         if (n <= 0)
             return false;
+
+        if (n == 1)
+            return true;
 
         while (n > 4)
         {
