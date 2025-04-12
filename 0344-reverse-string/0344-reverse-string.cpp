2c4dcf465c40d9f626bec8683e41d28087a5b463
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-12 13:39:59 +0900

    Time: 0 ms (100%), Space: 27.1 MB (80.58%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        unsigned short sSize = s.size();
+        unsigned short loopEnd = s.size() >> 1;
+
+        for (unsigned short i = 0; i < loopEnd; ++i)
+        {
+            char temp = s[i];
+            s[i] = s[sSize - 1 - i];
+            s[sSize - 1 - i] = temp;
+        }
+    }
+};
