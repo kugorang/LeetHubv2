afa488f0df99a4ba294015c910cf9e6bf7266c2d
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-22 18:52:13 +0900

    Time: 3 ms (20.45%), Space: 11.6 MB (62.95%) - LeetHub

--- 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -3,7 +3,7 @@ public:
     int numOfStrings(vector<string>& patterns, string word) {
         unsigned char answer = 0;
 
-        for (auto& pattern : patterns)
+        for (auto &pattern : patterns)
             if (word.find(pattern) != string::npos)
                 ++answer;
         
