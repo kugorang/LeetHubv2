7a506f0b316a6c8170bb04a9ac75fd4e6c0fd22e
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-27 07:45:25 +0900

    Time: 49 ms (8.5%), Space: 31 MB (97.49%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -5,10 +5,8 @@ public:
 
         for (auto &num : nums1)
             if (find(nums2.begin(), nums2.end(), num) == nums2.end())
-                answer[0].push_back(num);
-        
-        sort(answer[0].begin(), answer[0].end());
-        answer[0].erase(unique(answer[0].begin(), answer[0].end()), answer[0].end());
+                if (find(answer[0].begin(), answer[0].end(), num) == answer[0].end())
+                    answer[0].push_back(num);
         
         for (auto &num : nums2)
             if (find(nums1.begin(), nums1.end(), num) == nums1.end())
