a10e2bfbeff48712a497b3a006d9a6cca233dcb3
Kim Hyeonwoo <ialskdji@gmail.com>
2025-04-27 07:51:58 +0900

    Time: 49 ms (8.5%), Space: 31 MB (97.49%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -3,9 +3,6 @@ public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>> answer(2);
 
-        answer[0].reserve(nums2.size());
-        answer[1].reserve(nums1.size());
-
         for (auto &num : nums1)
             if (find(nums2.begin(), nums2.end(), num) == nums2.end())
                 if (find(answer[0].begin(), answer[0].end(), num) == answer[0].end())
@@ -13,8 +10,10 @@ public:
         
         for (auto &num : nums2)
             if (find(nums1.begin(), nums1.end(), num) == nums1.end())
-                if (find(answer[1].begin(), answer[1].end(), num) == answer[1].end())
-                    answer[1].push_back(num);
+                answer[1].push_back(num);
+        
+        sort(answer[1].begin(), answer[1].end());
+        answer[1].erase(unique(answer[1].begin(), answer[1].end()), answer[1].end());
 
         return answer;
     }
