63f13dad2344b8a7ccc6954cfd1fb8841fd0b073
Kim Hyeonwoo <ialskdji@gmail.com>
2025-06-19 21:30:09 +0900

    Time: 0 ms (100%), Space: 12 MB (65.35%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -5,11 +5,11 @@ public:
 
         for (string str : operations)
         {
-            if (str == "D")
+            if (str.compare("D") == 0)
                 temp.push_back(temp.back() << 1);
-            else if (str == "C")
+            else if (str.compare("C") == 0)
                 temp.pop_back();
-            else if (str == "+")
+            else if (str.compare("+") == 0)
                 temp.push_back(temp[temp.size() - 2] + temp.back());
             else
                 temp.push_back(stoi(str));
