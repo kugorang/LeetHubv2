d426e04377dabc7d43db588104c3b9562275afa5
Kim Hyeonwoo <ialskdji@gmail.com>
2025-07-21 23:24:17 +0900

    Time: 0 ms (100%), Space: 11.7 MB (95.54%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> occurrences;
+        sort(arr.begin(), arr.end());
+        int arrSize = arr.size();
 
-        for (int num : arr)
+        int prevNum = arr[0];
+        int numCount = 1;
+        vector<bool> checkNumCount(1001);
+
+        for (int i = 1; i < arrSize; ++i)
         {
-            if (occurrences.find(num) == occurrences.end())
-                occurrences.insert(make_pair(num, 1));
-            else
-                ++occurrences[num];
-        }
-
-        vector<pair<int, int>> occurVector(occurrences.begin(), occurrences.end());
-
-        sort(occurVector.begin(), occurVector.end(),
-            [](pair<int, int> a, pair<int, int> b)
+            if (arr[i] == prevNum)
             {
-                return a.second < b.second;
-            });
+                ++numCount;
+                continue;
+            }
 
-        int occurrencesSize = occurVector.size();
-        int prevSecond = occurVector[0].second;
-
-        for (int i = 1; i < occurrencesSize; ++i)
-        {
-            if (occurVector[i].second == prevSecond)
-                return false;
-            
-            prevSecond = occurVector[i].second;
+            if (checkNumCount[numCount])
+                    return false;
+                
+            checkNumCount[numCount] = true;
+            prevNum = arr[i];
+            numCount = 1;
         }
+
+        if (checkNumCount[numCount])
+            return false;
 
         return true;
     }
