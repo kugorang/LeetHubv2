e7d320c9d4e10c80aeecde846a2e961d273f3d84
Kim Hyeonwoo <ialskdji@gmail.com>
2025-07-27 23:32:22 +0900

    Time: 0 ms (100%), Space: 8 MB (100%) - LeetHub

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/1490-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string answer = "";;
+
+        if (n % 2 == 0)
+        {
+            answer.push_back('b');
+            --n;
+        }
+
+        for (int i = 0; i < n; ++i)
+            answer.push_back('a');
+
+        return answer;
+    }
+};
